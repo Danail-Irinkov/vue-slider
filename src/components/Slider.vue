ef8e345ca0fc597657aa169a4c80ef74f6672951
Dan <d.irinkovn1@gmail.com>
2020-08-05 18:47:19 +0300

    Adding Router Push functionality

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -26,127 +26,127 @@
 </template>
 
 <script>
-  import styles from '../assets/sass/variables.scss'
-  import utilities from '../js/utilities'
-  export default {
-    name: 'slider',
-    props: {
-      width: {
-        type: Number,
-        required: false,
-        default: 250
+import styles from '../assets/sass/variables.scss'
+import utilities from '../js/utilities'
+export default {
+  name: 'slider',
+  props: {
+    width: {
+      type: Number,
+      required: false,
+      default: 250
+    },
+    format: {
+      type: String,
+      required: false,
+      default: 'overlay',
+      validator: (value) => ['push', 'full', 'overlay'].indexOf(value) > -1
+    },
+    direction: {
+      type: String,
+      required: false,
+      default: 'left',
+      validator: (value) => ['left', 'right'].indexOf(value) > -1
+    },
+    opacity: {
+      type: Number,
+      required: false,
+      default: 0
+    },
+    links: {
+      type: Array,
+      required: false,
+      default: () => ([])
+    },
+    customStyles: {
+      type: Object,
+      required: false,
+      deep: true,
+      default: () => ({})
+    }
+  },
+  data () {
+    const defaultStyles = {
+      'navMenu': {},
+      'navIcon': {}
+    }
+
+    return {
+      styles: styles,
+      menuWidth: {
+        'width': 0
       },
-      format: {
-        type: String,
-        required: false,
-        default: 'overlay',
-        validator: (value) => ['push', 'full', 'overlay'].indexOf(value) > -1
-      },
-      direction: {
-        type: String,
-        required: false,
-        default: 'left',
-        validator: (value) => ['left', 'right'].indexOf(value) > -1
-      },
-      opacity: {
-        type: Number,
-        required: false,
-        default: 0
-      },
-      links: {
-        type: Array,
-        required: false,
-        default: () => ([])
-      },
-      customStyles: {
-        type: Object,
-        required: false,
-        deep: true,
-        default: () => ({})
+      userStyles: Object.assign({}, defaultStyles, this.customStyles)
+    }
+  },
+  watch: {
+    customStyles () {
+      this.userStyles = Object.assign({}, this.userStyles, this.customStyles)
+    }
+  },
+  computed: {
+    menuDirection () {
+      return this.direction === 'right'
+        ? { 'right': 0 }
+        : { 'left': 0 }
+    },
+    iconDirection () {
+      return { 'float': this.direction }
+    },
+    app () {
+      return document.getElementById('app')
+    }
+  },
+  mounted () {
+    console.log('vue-slider this.links', this.links)
+    if (!this.app) {
+      console.warn(`[Slider] The format was set to '${this.format}', but there is no element with an id 'app'. Add id='app' to the element to move.`)
+    }
+  },
+  methods: {
+    processRouterPush (link = false) {
+      console.log('processRouterPush', link)
+
+      if (link.routerPush && link.url === '#') {
+        this.$emit('processRouterPush', link.routerPush)
       }
     },
-    data () {
-      const defaultStyles = {
-        'navMenu': {},
-        'navIcon': {}
+    openMenu () {
+      const width = `${this.width}px`
+      const marginDirection = `margin-${this.direction}`
+
+      this.menuWidth = {
+        'width': this.format === 'full' ? '100%' : width
       }
 
-      return {
-        styles: styles,
-        menuWidth: {
-          'width': 0
-        },
-        userStyles: Object.assign({}, defaultStyles, this.customStyles)
+      if (this.app && this.format === 'push') {
+        this.app.style[marginDirection] = width
+        this.app.style.transition = `${marginDirection} .5s`
       }
-    },
-    watch: {
-      customStyles () {
-        this.userStyles = Object.assign({}, this.userStyles, this.customStyles)
+
+      if (this.opacity) {
+        document.body.style.background = utilities.hexToRGB(styles['background-color'], this.opacity)
       }
     },
-    computed: {
-      menuDirection () {
-        return this.direction === 'right'
-          ? { 'right': 0 }
-          : { 'left': 0 }
-      },
-      iconDirection () {
-        return { 'float': this.direction }
-      },
-      app () {
-        return document.getElementById('app')
+    closeMenu () {
+      this.menuWidth = { 'width': 0 }
+      if (this.app) {
+        this.app.style.marginLeft = '0'
+        this.app.style.marginRight = '0'
+      }
+      if (this.opacity) {
+        document.body.style.background = styles['background-color']
       }
     },
-    mounted () {
-      console.log('vue-slider this.links', this.links)
-      if (!this.app) {
-        console.warn(`[Slider] The format was set to '${this.format}', but there is no element with an id 'app'. Add id='app' to the element to move.`)
-      }
-    },
-    methods: {
-      processRouterPush (link = false) {
-        console.log('processRouterPush', link)
-
-        if (link.routerPush && link.url === '#') {
-          this.$emit('processRouterPush', link.routerPush)
-        }
-      },
-      openMenu () {
-        const width = `${this.width}px`
-        const marginDirection = `margin-${this.direction}`
-
-        this.menuWidth = {
-          'width': this.format === 'full' ? '100%' : width
-        }
-
-        if (this.app && this.format === 'push') {
-          this.app.style[marginDirection] = width
-          this.app.style.transition = `${marginDirection} .5s`
-        }
-
-        if (this.opacity) {
-          document.body.style.background = utilities.hexToRGB(styles['background-color'], this.opacity)
-        }
-      },
-      closeMenu () {
-        this.menuWidth = { 'width': 0 }
-        if (this.app) {
-          this.app.style.marginLeft = '0'
-          this.app.style.marginRight = '0'
-        }
-        if (this.opacity) {
-          document.body.style.background = styles['background-color']
-        }
-      },
-      toggleMenu () {
-        if (this.menuWidth.width === 0) {
-          this.openMenu()
-        } else {
-          this.closeMenu()
-        }
+    toggleMenu () {
+      if (this.menuWidth.width === 0) {
+        this.openMenu()
+      } else {
+        this.closeMenu()
       }
     }
   }
+}
 </script>
 
 <style lang="scss" scoped></style>
